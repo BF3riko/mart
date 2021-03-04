54ed849759d2c794dd52e7996dd4dbce51163680
n.fataliev <n.fataliev@spherelab.ru>
2021-03-04 14:44:45 +0300

    Колесо

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Winwheel from './component/Winwheel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Winwheel />
     </div>
   );
 }
