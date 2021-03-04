ee38abe37f998bb23e679ce12b0220f4eada6ddd
n.fataliev <n.fataliev@spherelab.ru>
2021-03-04 18:52:21 +0300

    Табло

--- src/component/Wall/Wall.jsx
+++ src/component/Wall/Wall.jsx
@@ -5,6 +5,21 @@ import Style from './css/style.module.css';
 class Wall extends Component {
   state = {
     squares: 125,
+    letterM: {
+      index: 35,
+      letter: 'M',
+    },
+    letterP: {
+      index: 37,
+      letter: 'Р',
+    },
+    letterA: {
+      index: 39,
+      letter: 'А',
+    },
+    superFinalArr: 'СУПЕРФИНАЛ'.split(''),
+    numberEight: [2, 3, 4, 27, 29, 36, 38, 52, 53, 54, 77, 79, 102, 103, 104],
+    wordIndices: [108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
   }
 
   splitting = (arr) => {
@@ -18,10 +33,44 @@ class Wall extends Component {
   }
 
   paintingDigit = (index) => {
-    let arrIndex = [2, 3, 4, 27, 29, 52, 53, 54, 77, 79, 102, 103, 104];
-    for (const number of arrIndex) {
+    for (const number of this.state.numberEight) {
       if (number === index) {
         return <div className={Style.square} style={{backgroundColor: '#000000'}}></div>;
+      }
+    }
+
+    if (index === this.state.letterM.index) {
+      return (
+        <div className={Style.square}>
+          <p>{this.state.letterM.letter}</p>
+        </div>
+      );
+    }
+
+    if (index === this.state.letterP.index) {
+      return (
+        <div className={Style.square}>
+          <p>{this.state.letterP.letter}</p>
+        </div>
+      );
+    }
+
+    if (index === this.state.letterA.index) {
+      return (
+        <div className={Style.square}>
+          <p>{this.state.letterA.letter}</p>
+        </div>
+      );
+    }
+
+    for (const number of this.state.wordIndices) {
+      let i = this.state.wordIndices.indexOf(number);
+      if (number === index) {
+        return (
+          <div className={Style.square}>
+            <p>{this.state.superFinalArr[i]}</p>
+          </div>
+        );
       }
     }
 
