5b469d6f3da47768d8e3dece577f723cd2c3ad95
v.glukhov <v.glukhov@spherelab.ru>
2021-03-04 22:49:05 +0300

    added png person

--- src/component/Scene/Scene.jsx
+++ src/component/Scene/Scene.jsx
@@ -5,18 +5,16 @@ import Character from "../Character";
 
 import Wall from "../Wall/"; //Табло
 import Ceiling from "../Ceiling"; //Потолок
-import Window from '../Window'; //Окно справа в углу
+import Window from "../Window"; //Окно справа в углу
 
 import Style from "./css/style.module.css";
 
 import bg from "../../img/якубович.jpg";
+import bg_rabbit from "../../img/Ilgiz1.png";
+import bg_egg from "../../img/Ilgiz2.png";
 
-import bg_rabbit from "../../img/заяц.jpg";
-
-import bg_egg from "../../img/pngegg.png";
 import Side from "../Side";
 import Floor from "../Floor";
-
 
 const phases = {
   START: "start",
@@ -58,28 +56,28 @@ class Scene extends Component {
     return (
       <div className={Style.scene}>
         <Side />
-        <Side second={true}/>
+        <Side second={true} />
         <Ceiling />
         <Wall />
         <Winwheel sendResult={this.sendResult} />
-        <Character posX={100} posY={100} width={300} height={300} bg={bg_egg} />
+        <Character posX={5} posY={20} width={300} height={550} bg={bg_egg} />
         <Character
-          posX={900}
-          posY={100}
-          posEndX={800}
-          posEndY={100}
+          posX={45}
+          posY={20}
+          posEndX={30}
+          posEndY={20}
           width={300}
-          height={300}
+          height={500}
           bg={bg_rabbit}
           transitionTime={0.2}
           isAnimated={this.state.phase === phases.KICK}
         />
 
         <RandomedCharacter
-          posX={-300}
-          posY={100}
-          posEndX={500}
-          posEndY={500}
+          posX={100}
+          posY={10}
+          posEndX={40}
+          posEndY={50}
           width={300}
           height={300}
           transitionTime={3}
@@ -87,7 +85,6 @@ class Scene extends Component {
           isAnimated={this.state.phase === phases.END}
         />
 
-        <Character posX={400} posY={700} width={300} height={300} bg={bg} />
         <Floor />
         <Window />
       </div>
