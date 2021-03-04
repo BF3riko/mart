df52f09f84c3777367d5d716abf20ea558f75d6b
n.fataliev <n.fataliev@spherelab.ru>
2021-03-04 16:42:54 +0300

    правки барабана

---
+++ src/icons/Arrow.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Arrow = (props) => {
+    const { className } = props;
+
+    return (
+        <svg className={className} viewBox="0 0 56 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M20.9249 6.74201L0.632292 0.641866L12.6916 18.0652L15.6321 14.0212L52.8368 41.0732L55.1891 37.838L17.9844 10.786L20.9249 6.74201Z" fill="#F2B71D"/>
+        </svg>
+    )
+}
+
+export default Arrow;
